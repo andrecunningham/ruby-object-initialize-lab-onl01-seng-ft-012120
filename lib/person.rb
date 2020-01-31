a97b0a23805534ce58cf857e77a2715a629cd06b
Andre Cunningham <andrecunningham62@gmail.com>
2020-01-31 01:00:21 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,3 @@
 describe 'Person' do
   describe '#initialize' do
-    it 'sets the name of the person in an instance variable @name' do
-      beyonce = Person.new("Andre")
-
-      expect(beyonce.instance_variable_get(:@name)).to eq("Andre")
-    end
-  end
-end
+   
